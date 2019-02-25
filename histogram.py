2b467b106b3215aeb40087b13af734b5c2cc9b74
Ellis Lee <ellismckenzielee@gmail.com>
2019-02-25 10:54:28 +0000

    compelted histogram kata

---
+++ histogram.py
@@ -0,0 +1,23 @@
+import numpy as np
+def hist(s):
+    '''Builds upon printing error kata. Returns a histogram of the errors in:
+        -Type of error
+        -Error counts
+        -Error count in starts
+        format.'''
+
+    print_list = list(s)
+    unique, counts = np.unique(print_list, return_counts=True)
+    dictionary_counts = dict(zip(unique, counts))
+    output_string = ''
+    for letter in ['u', 'w', 'x', 'z']:
+        try:
+            stars = dictionary_counts[letter] * '*'
+            if dictionary_counts[letter] >= 10:
+                space = '    '
+            else: 
+                space = '     '
+            output_string = output_string + letter + '  ' + str(dictionary_counts[letter]) + space + stars + '\r' 
+        except:
+            pass
+    return output_string[:-1]
