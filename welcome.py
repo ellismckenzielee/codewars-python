1cbfeac394465587288d42ca950372bac8b528be
Ellis Lee <ellismckenzielee@gmail.com>
2019-04-06 19:35:15 +0100

    completed welcome kata

---
+++ welcome.py
@@ -0,0 +1,20 @@
+def greet(language):
+    '''Returns welcome in language depending on origin of person'''
+    languages = {'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'}
+    return languages[language] if language in languages.keys() else 'Welcome'
