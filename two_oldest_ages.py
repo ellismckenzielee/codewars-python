ecf787b606aec8002495873c9647a98a3f0e6635
ellismckenzielee <ellismckenzielee@gmail.com>
2020-02-23 21:21:56 +0000

    completed two-oldest-ages kata

---
+++ two_oldest_ages.py
@@ -0,0 +1,6 @@
+###Two Oldest Ages - codewars kata
+###https://www.codewars.com/kata/511f11d355fe575d2c000001/python
+
+def two_oldest_ages(ages):
+    '''Return the two largest numbers, from smallest to largest'''
+    return sorted(ages)[-2:]
