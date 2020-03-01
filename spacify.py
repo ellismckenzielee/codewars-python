92886f5094c104d0ada2daf9080e25a99e5ca547
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-01 22:26:33 +0000

    completed spacify kata

---
+++ spacify.py
@@ -0,0 +1,6 @@
+###Spacify - codewars kata
+###https://www.codewars.com/kata/57f8ee485cae443c4d000127
+
+def spacify(string):
+    '''Returns string but spacified'''
+    return ' '.join(list(string))
