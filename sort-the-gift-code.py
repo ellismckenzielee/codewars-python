27b7044402e6b51e07260bc72039de9cdc08db94
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-04 23:31:34 +0000

    completed sort-the-gift-code kata

---
+++ sort-the-gift-code.py
@@ -0,0 +1,6 @@
+###Sort the Gift Code - codewars kata
+###https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3
+
+def sort_gift_code(code):
+    '''Sort and return the code in alphabetical order'''
+    return ''.join(sorted(code))
