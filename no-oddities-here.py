4c64b2e5f57c90d4fc2f52270beb4fc94ac9707f
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-08 22:50:03 +0000

    completed no-oddities-here kata

---
+++ no-oddities-here.py
@@ -0,0 +1,7 @@
+###No oddities here - codewars kata
+### https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/python
+
+def no_odds(values):
+    '''Returns a list of even values from values array'''
+    return [value for value in values if value % 2 == 0]
+    
