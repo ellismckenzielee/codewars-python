41908dc184279ef2c35ec93b35ad25a83d3e88b6
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-31 21:39:12 +0100

    completed genetic-algorithm-series#3 kata

---
+++ genetic-algorithm-series#3.py
@@ -0,0 +1,6 @@
+###Genetic Algorithm Series - #3 Crossover - codewars kata
+###https://www.codewars.com/kata/567d71b93f8a50f461000019/solutions/python
+
+def crossover(chromosome1, chromosome2, index):
+    '''Simulates a genetic crossover of chromosomes 1&2 at point index'''
+    return [chromosome1[:index] + chromosome2[index:], chromosome2[:index] + chromosome1[index:]]
