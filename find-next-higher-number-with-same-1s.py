a031112d7f3c2b9a50d2754a972cd9973c0acded
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-31 21:23:31 +0100

    completed find-next-higher-number-with-same-1s kata

---
+++ find-next-higher-number-with-same-1s.py
@@ -0,0 +1,13 @@
+###Basics 08: Find next higher number with same Bits (1's) - codewars kata
+###https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5
+
+def next_higher(value):
+    '''Returns the next number above value which has the same 
+    number of ones when displayed in binary'''
+    initial = bin(value).split('b')[1].count('1')
+    current_num = value+1 
+    while True:
+        number_1s = bin(current_num).split('b')[1].count('1')
+        if number_1s == initial:
+            return current_num
+        current_num += 1
