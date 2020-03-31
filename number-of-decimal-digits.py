204836fabf5c5ec3918b4c1801a4183321675f3b
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-31 21:40:27 +0100

    completed number-of-decimal-digits kata

---
+++ number-of-decimal-digits.py
@@ -0,0 +1,6 @@
+###Number of Decimal Digits - codewars kata
+###https://www.codewars.com/kata/58fa273ca6d84c158e000052
+
+def digits(n):
+    '''Returns the number of digits in n'''
+    return str(n).__len__()
