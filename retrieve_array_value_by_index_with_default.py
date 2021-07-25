1695f3ef86a9663822279a8f176cbad11362383a
Ellis Lee <ellismckenzielee@gmail.com>
2021-07-25 14:18:07 +0100

    completed retrieve array value by index with default kata

---
+++ retrieve_array_value_by_index_with_default.py
@@ -0,0 +1,8 @@
+#Retrieve array value by index with default
+#https://www.codewars.com/kata/515ceaebcc1dde8870000001
+
+def solution(items, index, default_value):
+    try:
+        return items[index]
+    except:
+        return default_value    
