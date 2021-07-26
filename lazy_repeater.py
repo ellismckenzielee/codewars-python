abb354846227c75cc74db12066acbcfe1a7c46db
Ellis Lee <ellismckenzielee@gmail.com>
2021-07-26 13:40:47 +0100

    completed lazy repeater kata

---
+++ lazy_repeater.py
@@ -0,0 +1,13 @@
+#Lazy Repeater kata
+#https://www.codewars.com/kata/51fc3beb41ecc97ee20000c3
+
+def make_looper(string):
+    global x
+    x = list(string)
+    return func
+
+def func():
+    y = x.pop(0)
+    x.append(y)
+    return y
+    
