3eaa65443778a2a604bc69715b13baac55531c76
Ellis Lee <ellismckenzielee@gmail.com>
2021-08-13 18:13:16 +0100

    completed absent vowel kata

---
+++ absent_vowel.py
@@ -0,0 +1,8 @@
+#absent vowel
+#https://www.codewars.com/kata/56414fdc6488ee99db00002c
+
+def absent_vowel(x): 
+    letters = set(x)
+    for i, vowel in enumerate(['a','e','i','o','u']):
+        if vowel not in letters:
+            return i
