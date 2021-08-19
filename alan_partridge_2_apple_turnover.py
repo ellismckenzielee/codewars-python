7056c62df84d96b2741cfd9a7bf3f9988e9336fb
Ellis Lee <ellismckenzielee@gmail.com>
2021-08-19 11:28:45 +0100

    completed alan partridge 2 - apple turnover kata

---
+++ alan_partridge_2_apple_turnover.py
@@ -0,0 +1,5 @@
+#alan partridge 2 - apple turnover kata
+#https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+def apple(x):
+    return 'It\'s hotter than the sun!!'  if int(x)**2 > 1000 else  "Help yourself to a honeycomb Yorkie for the glovebox."
