ae09ce40ab38bf984f8360d09f6470f9c1292257
Ellis Lee <ellismckenzielee@gmail.com>
2021-08-19 11:38:48 +0100

    completed how many lightsabers do you own? kata

---
+++ how_many_lightsabers_do_you_own.py
@@ -0,0 +1,5 @@
+#how many lightsabers do you own? kata
+#https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+
+def how_many_light_sabers_do_you_own(name='stan'):
+    return 18 if name == 'Zach' else 0
